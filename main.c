2c833222ee160b4e529f78e2330dfe539a32929d
Lisamarie Treser <ltreser@c1b3c2.42berlin.de>
2024-10-10 17:31:54 +0200

    chore: pseudo code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ltreser <ltreser@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 15:42:49 by ltreser           #+#    #+#             */
-/*   Updated: 2024/10/10 16:08:44 by ltreser          ###   ########.fr       */
+/*   Updated: 2024/10/10 17:31:30 by ltreser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,12 @@ int	main(int ac, char **av)
 {
 	(void)av;
 	if (5 == ac || 6 == ac)
-		printf("asdf\n");
+	{
+		//error checking
+		//saving input data
+		//initialise data
+		//simulation exec
+		//clear table (no leaks)
 	else
 		return (printf("%s", WRONG_INPUT), exit(EXIT_FAILURE), 1);
 }
