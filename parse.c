ce767322f67baddc21ddf309aee4ae05cd4e0aca
Lisamarie Treser <ltreser@c4c6c5.42berlin.de>
2024-10-20 17:25:15 +0200

    feat: error handling

--- parse.c
+++ parse.c
@@ -2,8 +2,21 @@
 
 int	reservation_correct(int ac, char **av)
 {
+	int i;
+
+	i = 0;
 	if (ac < 5 && ac > 6)
 		return (0);
+	if (ac == 6 && !isnum_m(av[5] + 1, ft_strlen(av[5] - 2)))
+		return (0);
+	if (ac == 6 && !(av[5][0] == '[' && av[5][ft_strlen(av[5] - 1)] == ']'))
+		return (0);
+	while (++i < 5)
+		if (!isnum_n(av[i], -1))
+			return (0);
+	
+	
+		
 	//input actually numbers -> only positive
 	//no bigger than max int
 	//timestamps > 60ms
--- utils.c
+++ utils.c
@@ -1,6 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltreser <ltreser@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 16:42:28 by ltreser           #+#    #+#             */
+/*   Updated: 2024/10/20 17:24:46 by ltreser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
+
+long	ft_atol(const char *str)
+{
+	long	n;
+	
+	n = 0;
+	str = 
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
 int	isnum_n(char *str, int n)
 {
+	int i;
 
+	i = 0;
+	if (n == '-1')
+		n = ft_strlen(str);
+	while (n--)
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	return (1);
+}
 
